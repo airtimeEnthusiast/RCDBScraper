0e29fff2986b715c600c18a7e5147aa942379355
Austin Wright <9201assumplon9201@gmail.com>
2022-01-07 15:32:17 -0700

    building the state scraper tester

--- RCDB_Scraper.py
+++ RCDB_Scraper.py
@@ -1,20 +1,6 @@
-#from bs4 import BeautifulSoup
-#import requests
-#import csv
-#import pandas as pd
-
-#Testing a wiki table
-#page = requests.get('https://en.wikipedia.org/wiki/Metropolitan_statistical_area')
-#soup = BeautifulSoup(page.text, 'html.parser')
-#table = soup.findAll('table',{'class':"wikitable"})
-#table_soup = soup.findAll('table')[0].findAll('tr')
-#df = pd.read_html(str(table))
-#df = pd.DataFrame(df[0])
-#df.drop(["Rank"],axis=1)
-#df.to_csv('metros.csv')
-
-#Inspired by Will Clifford's RCDB Scraper all rights go to his code
-#https://github.com/willcliffy/RCDB-Scraper/blob/d182ff4121b0d81e761e7b96fe038b2c1a1c5782/scraper.py#L121
+## RCDB WebScraper ##
+#Created by: Austin Wright
+#Creates a CSV of coasters stats
 
 import time
 import requests
@@ -22,53 +8,38 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 import enum
 
-#Roller Coaster Data Base Scraper Class
-class RCDB_Parser(enum.Enum):
-  
-  def parse_page(soup):
-    meta = soup.body.section.div.div
-    Name = meta.div.h1.string
-    
-    tmp = meta.div.find_all("a")
-    
-    if len(tmp) < 4:
-      return 0
-    
-    Park = tmp[-4].string
-    City = tmp[-3].string
-    State = tmp[-2].string
-    Country = tmp[-1].string
-    
-    OperatingSince = None
-    OperatingUntil = None
-    
-    Status = meta.p.a.string
-    
-    if Status == "Operating":
-      if meta.p.time != None:
-        OperatingSince = meta.p.time['datetime']
-      elif Status == "Operated":
-        tmp = meta.p.find_all('time')
-        if len(tmp) == 2:
-          OpSince = tmp[0]['datatime']
-          OpUntil = tmp[1]['datatime']
-      elif Status == "Under Construction":
-        pass
-      elif Status == "In Business":
-        pass
+#parse the coasters found on each state page
+def parse_state_page(link,name):
+  print("Parse " + name + " at "  + link)
+  response = requests.get(link)
+  soup = bs(response.text,"html.parser")
+  time.sleep(10)
+  extant_ref = soup.find("body").find("table", id_="counts")
+  print(extant_ref)
 
 
+#test the parse_state_page
+def parse_state_page_tester():
+  link = "https://rcdb.com/location.htm?id=13833"
+  name = "Nevada"
+  response = requests.get(link)
+  soup = bs(response.text, "html.parser")
+
+
+#parse the pages found on the US List of coasters on RCDB 
 def parse_pages():
-    us_url = "https://rcdb.com/location.htm?id=59"
-    response = requests.get(us_url)
-    soup = bs(response.text, "html.parser")
-    states_table = soup.find("body").find("div", class_="stdtbl cen").find("table").find("tbody").find_all("tr")
-    for state in states_table:
-      table_data = state.find_all("td")
-      ref = table_data[0].find("a")
-      print(ref)
-
-    
+    us_url = "https://rcdb.com/location.htm?id=59" #List of Coasters in the US
+    response = requests.get(us_url)                           #Create the response 
+    soup = bs(response.text, "html.parser")             #Parse the response
+    states_table = soup.find("body").find("div", class_="stdtbl cen").find("table").find("tbody").find_all("tr")  #Find the table of states
+    for state in states_table:                #Parse each state
+      table_data = state.find_all("td") #State table data sections
+      td = table_data[0].find("a")          #State name and link section
+      ref = "https://rcdb.com" + td.get('href') #Link reference
+      name = td.get_text()                        #State name
+      parse_state_page(ref,name)
+      print("next state...")
+      time.sleep(10)
     
 
 def main():
